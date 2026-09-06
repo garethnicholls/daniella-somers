9bdc05de200d3c1a16802ea9c0b20923c3209b89
garethnicholls <garethnicholls1992@hotmail.com>
2026-09-06 15:57:20 +0100

    Load exact-design overrides

--- wp-content/themes/daniella-somers/functions.php
+++ wp-content/themes/daniella-somers/functions.php
@@ -3,17 +3,14 @@
 if (!defined('ABSPATH')) { exit; }
 
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style(
-        'daniella-somers',
-        get_stylesheet_uri(),
-        array(),
-        wp_get_theme()->get('Version')
-    );
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('daniella-somers', get_stylesheet_uri(), array(), $version);
+    wp_enqueue_style('daniella-somers-exact', get_template_directory_uri() . '/exact.css', array('daniella-somers'), $version);
 });
 
 add_action('after_setup_theme', function () {
     add_theme_support('wp-block-styles');
     add_theme_support('responsive-embeds');
     add_theme_support('editor-styles');
-    add_editor_style('style.css');
+    add_editor_style(array('style.css', 'exact.css'));
 });
